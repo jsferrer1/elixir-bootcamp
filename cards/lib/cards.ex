a9442cfb855fa7bb3facef6df6a61ca4fa1d37d9
jsferrer1 <jsferrer1@yahoo.com>
2020-06-22 20:50:01 +0800

    create_desk with suits

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -11,7 +11,12 @@ defmodule Cards do
   """
 
   def create_deck do
-    ["Ace", "Two", "Three", "Four", "Five"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
