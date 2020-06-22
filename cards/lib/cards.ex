a3a6acd3a60eef0e8be616931a82d5983979d316
jsferrer1 <jsferrer1@yahoo.com>
2020-06-22 19:42:43 +0800

    Initial create_deck function

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,19 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for `Cards`.
+  Provides methods for creating and handling a deck of cards
   """
 
   @doc """
-  Hello world.
+  Returns a list of strings representing a deck of playing cards
 
   ## Examples
-
       iex> Cards.hello()
-      :world
-
   """
-  def hello do
-    :world
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    for value <- values do
+      value
+    end
   end
 end
