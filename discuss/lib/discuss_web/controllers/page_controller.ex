7affa6d3cdeb91a9b27c33559c044d018caf36ad
jsferrer1 <jsferrer1@yahoo.com>
2020-06-24 19:52:24 +0800

    Delete the page controller, template, view

--- discuss/lib/discuss_web/controllers/page_controller.ex
+++
@@ -1,8 +0,0 @@
-defmodule DiscussWeb.PageController do
-  use DiscussWeb, :controller
-
-  def index(conn, _params) do
-    IO.puts "page_controller: index"
-    render(conn, "index.html")
-  end
-end
