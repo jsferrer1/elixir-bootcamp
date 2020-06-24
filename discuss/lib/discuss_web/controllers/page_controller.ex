54b3d3932f0a4419c2fc51dcddd0c9ed26a2e334
jsferrer1 <jsferrer1@yahoo.com>
2020-06-24 18:00:06 +0800

    TopicController create to insert changeset

--- discuss/lib/discuss_web/controllers/page_controller.ex
+++ discuss/lib/discuss_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule DiscussWeb.PageController do
   use DiscussWeb, :controller
 
   def index(conn, _params) do
+    IO.puts "page_controller: index"
     render(conn, "index.html")
   end
 end
